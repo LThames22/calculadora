3a153ff51d1f686fb79fbed49b885aedb7d0bd46
LThames22 <likrein22@gmail.com>
2021-09-27 14:14:34 -0300

    asd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const math = require('mathjs')
 
 app.use(express.json())
@@ -10,5 +10,5 @@ app.use('/numeros',require('./endpoint/numeros'))
 app.use('/matriz',require('./endpoint/matriz'))
 
 app.listen(port,()=>{
-    console.log('Escuchando puerto 5000')
+    console.log(`app running on ${port}`)
 })
